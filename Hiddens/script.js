04466130e224bdcb5f2c5a2320e8fa9dc90c411c
G-xming <GitHub@emailed.cf>
2023-05-01 12:16:51 -0500

    ff
    f

--- Hiddens/script.js
+++ Hiddens/script.js
@@ -7,6 +7,12 @@ function btnclick() {
     iframe.style.width = '100%';
     iframe.style.height = '100%';
     iframe.style.margin = '0';
-    iframe.src = "https://unres.viewa.website/unrestricted.html";
+/*  iframe.src = "https://example.com"; */
+    iframe.src = "file:///home/chronos/u-a6403f850ebd9c9ddd0e13b353dd2da93a78c2d0/MyFiles/Page/unrestricted.html";
     win.document.body.appendChild(iframe);
+/*  win.document.title = "alphabet"
+    var abtitle = win.document.createElement('title');
+    abtitle.innerHTML = "Home | ";
+    win.document.body.appendChild(abtitle);
+*/
 }
